18270700a7ecfc52ced5e6db76adfe6ecc99a862
sosthene <sothenetimi94@gmail.com>
2023-05-17 03:29:33 -0700

    adding uniq numbers in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    if my_list is None:
-        return 0
+    summation = 0
     for num in my_list:
         summation = sum(set(my_list))
     return summation
