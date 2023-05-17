283f7c670c69dfeca05ed3695d3f3bdb9cb4fbbc
sosthene <sothenetimi94@gmail.com>
2023-05-17 02:49:49 -0700

    computing the square of all integers in a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    columns = len(matrix[0])
-    rows = len(matrix)
-    newest_matrix = [[0 for k in range(columns)] for j in range(rows)]
-    for j in range(rows):
-        for i in range(columns):
-            newest_matrix[i][j] = matrix[i][j] ** 2
-    return newest_matrix
+    new_matrix = [[num ** 2 for num in row] for row in matrix]
+    return new_matrix
