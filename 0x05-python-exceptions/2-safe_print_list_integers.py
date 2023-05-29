bc25c877745975e5dbe7589502a09f922dfc361f
T-sosthenese <sosthenetimi94@gmail.com>
2023-05-29 14:23:16 +0300

    rinting the first x elements of a list consisting integers only

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     printed_count = 0
-    try:
-        for i in range(0, x):
+    for i in range(0, x):
+        try:
             print("{:d}".format(my_list[i], end=""))
-            printed_count + 1
-    except (TypeError, ValueError):
-        pass
-
-        print()
-        return printed_count
+            printed_count += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return printed_count
