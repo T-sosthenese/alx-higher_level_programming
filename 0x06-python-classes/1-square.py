dd5dcf9d505f7b306044c7a3ee261eab48d2ea0d
T-sosthenese <sosthenetimi94@gmail.com>
2023-05-30 15:22:04 +0300

    adding private size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/in/python3
+"""
+module documetation
+"""
+
+
+class Square():
+    """
+    inserting private instance size
+    """
+    def __init__(self, size):
+        self.__size = size
