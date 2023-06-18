a29d875029e1422cff7b3731b1295462985a84d7
T-sosthenese <sosthenetimi94@gmail.com>
2023-06-18 12:53:55 +0300

    square setting and validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,3 +18,26 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
         self.size = size
+
+    @property
+    def size(self):
+        """
+        Getter for square size.
+        Returns size of the width
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the size of square sides equal to size.
+        """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """
+        Overrides built-in __str__ to print a string in a specified format
+        """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
+                                                         self.y, self.width)
