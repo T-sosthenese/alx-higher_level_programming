64d989158b01a391630dfc05b917f361dfeda132
T-sosthenese <sosthenetimi94@gmail.com>
2023-07-18 21:19:02 +0300

    Average city temperatures

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- display average temperature(farenheit) by city ordered by temperature (descending)
+SELECT `city`, AVG(`value`) AS avg_temp FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
