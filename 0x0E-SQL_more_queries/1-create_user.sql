84a6656eb0cdb19f4eeabf4f20b4bffb8043e4c3
T-sosthenese <sosthenetimi94@gmail.com>
2023-07-19 23:08:01 +0300

    granting more privileges to our user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- A script that creates 'user_0d_1' user 
+-- Gives the user above all the provileges
+-- sets the password for 'user_0d_1' to 'user_0d_1_pwd'
+-- Does not fail if 'user_0d_1' exists
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
