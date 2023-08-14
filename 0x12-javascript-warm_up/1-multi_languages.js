1437edde99b6ea1124366858b140063821301a22
sosthene <sosthenetimi94@gmail.com>
2023-08-14 14:34:43 +0300

    Language descriptions

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myString1 = 'C is fun';
+const myString2 = 'Python is cool';
+const myString3 = 'JavaScript is amazing';
+
+console.log(myString1);
+console.log(myString2);
+console.log(myString3);
