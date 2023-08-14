beb1827d933bae4663f595617bbffb816631a01a
sosthene <sosthenetimi94@gmail.com>
2023-08-14 18:01:33 +0300

    Factorials

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function factorial (number) {
+  if (number === 1) {
+    return 1;
+  } else if (number > 1) {
+    return number * (factorial(number - 1));
+  }
+}
+
+const myNumber = parseInt(process.argv[2]);
+if (isNaN(process.argv[2])) {
+  console.log(1);
+} else {
+  console.log(factorial(myNumber));
+}
