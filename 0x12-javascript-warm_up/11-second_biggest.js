a0c338d06f4b95f8afbf9dfc16cc3353d24283db
sosthene <sosthenetimi94@gmail.com>
2023-08-14 18:41:25 +0300

    Is the second largest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const arrayNums = [];
 
 const lengthNumbers = process.argv.length;
-if (lengthNumbers < 4) {
+if (lengthNumbers === 2 || lengthNumbers === 3) {
   console.log(0);
 } else {
   for (let i = 2; i < lengthNumbers; i++) {
