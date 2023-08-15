4688f48558829aae55a5f17ed237d81f6b6bd4f5
sosthene <sosthenetimi94@gmail.com>
2023-08-15 15:13:56 +0300

    Printing the rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let stringRectangle = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        stringRectangle += 'X';
+      }
+      if (i < this.height) {
+        stringRectangle += '\n';
+      }
+    }
+    console.log(stringRectangle);
+  }
+}
+
+module.exports = Rectangle;
