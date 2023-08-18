eb81e0954598b40c50fea57268c18ed5a960d2f2
sosthene <sosthenetimi94@gmail.com>
2023-08-18 11:29:18 +0300

    Searching states by args

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
                          host='localhost', port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:S}' ORDER BY id ASC".
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".
                 format(sys.argv[4]))
     states = cur.fetchall()
     for state in states:
