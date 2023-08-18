ff226d945a3ec8c3a7b0e1123defbaacebb3253d
sosthene <sosthenetimi94@gmail.com>
2023-08-18 11:06:05 +0300

    Filtering states by first letter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],\
                          host='localhost', port=3306)
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
+    query = "SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY `id`"
     cur.execute(query)
     [print(state) for state in cur.fetchall()]
     cur.close()
