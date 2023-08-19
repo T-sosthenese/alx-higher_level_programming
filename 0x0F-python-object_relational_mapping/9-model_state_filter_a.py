ff71ab4e3cd95793029f61a1263b49f9dc7b1e99
sosthene <sosthenetimi94@gmail.com>
2023-08-19 12:08:26 +0300

    States with "a"

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Displaying all states with letter 'a'
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+if __name__ == '__main__':
+    # Creating a SQLAlchemy engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1],
+                           sys.argv[2],
+                           sys.argv[3],
+                           pool_pre_ping=True))
+    # Creating a configure session class
+    Session = sessionmaker(bind=engine)
+    # Creating a session
+    session = Session()
+    # Querying and displaying the objects
+    states = session.query(State).filter(State.name.like('%a%')).order_by(
+                                         State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    # Close session
+    session.close()
