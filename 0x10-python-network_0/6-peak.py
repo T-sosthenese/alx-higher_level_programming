e22912ea9ee4297cca0b60dc0e1094d02abc2a06
sosthene <sosthenetimi94@gmail.com>
2023-08-31 16:11:38 +0300

    Peaking

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,9 +13,12 @@ def find_peak(list_of_integers):
         return None
 
     while (left < right):
+
         mid = (left + right) // 2
+
         if list_of_integers[mid] < list_of_integers[mid + 1]:
             left = mid + 1
         else:
             right = mid
+
     return list_of_integers[left]
