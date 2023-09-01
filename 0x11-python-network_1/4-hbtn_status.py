1f7bd5717b02615fdc5ccf0ac6adc690c63ada03
sosthene <sosthenetimi94@gmail.com>
2023-09-01 15:23:17 +0300

    displaying the body

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    req = request.get(url)
+    req = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
     print("\t- content: {}".format(req.text))
