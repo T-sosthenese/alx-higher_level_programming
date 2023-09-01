2e3220223b6361ee841a058bf4f801242a23ef81
sosthene <sosthenetimi94@gmail.com>
2023-09-01 16:01:26 +0300

    X-Request-Id with requests module

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,4 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     r = requests.get(url)
-    request_id = r.headers['X-Request-Id']
-    print(request_id)
+    print(r.headers.get("X-Request-Id"))
